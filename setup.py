33c5a8e6ca118c18a75cd9a574a459db4a9c8f5c
Miguel-Angel Monjas <mmonjas@gmail.com>
2019-02-03 19:53:43 +0100

    Fix in setup.py

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ setup(
     #   py_modules=["my_module"],
     #
     #packages=find_packages(exclude=['bin', 'include', 'lib', 'tests']),  # Required
-    packages=find_packages(exclude=[]),  # Required
+    packages=find_packages(exclude=['bin', 'include', 'lib']),  # Required
 
     # This field lists other packages that your project depends on to run.
     # Any package you put here will be installed by pip when your project is
